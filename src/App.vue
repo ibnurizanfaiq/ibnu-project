1f4d6eb8740410a6aeb51a4231d8fe972958ed55
ibnu <ekmalhafizhqoula987@gmail.com>
2024-04-05 13:38:30 +0700

    ibnu first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import navbar from './components/navbar.vue';
+</script>
+
+<template>
+  <div>
+    <navbar />
+  </div>
+  <div style="position: relative" class="main">
+      <h1>Hello,</h1>
+      <h1>I'M Ibnu Rizan Faiq</h1>
+      <span>AI and IOT</span><br />
+      <h4>I Am a Student in Universitas Islam Riau</h4><br>
+      <h4>Biodata</h4>
+      <p>
+        Nama : Ibnu Rizan Faiq<br>
+        Tempat Tanggal Lahir : Dayo 09-12-2003<br>
+        Status : Mahasiswa
+      </p>
+    <br />
+    <h4>Skill & Pengalaman</h4>
+    <div >
+      Konfigurasi Mikrotik
+      <br>Analisis Kerusakan Komputer
+      <br>Instalasi OS
+      <br>Pemasangan LAN<br>
+    </div>
+  </div>
+</template>
+
+<style >
+body{
+  background-image: url('web.jpeg');
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.main{
+  color: rgb(211, 208, 13);
+  text-transform: uppercases;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,57 @@
+<script setup>
+    
+</script>
+
+<template>
+
+<nav class="navtop">
+        <div>
+            <h1><i class="bi bi-cpu">Ibnu Rizan</i> </h1>
+            <v-btn text @click="scroll('home')"><i class="bi bi-house-door-fill"></i>Home</v-btn>
+            <v-btn text @click="scroll('about')" ><i class="bi bi-file-person"></i>About</v-btn>
+            <v-btn text @click="scroll('contact')"><i class="bi bi-telephone-fill"></i>Contact</v-btn>
+            <v-btn text @click="scroll('portfolio')"><i class="bi bi-person-standing"></i>Portfolio</v-btn>
+        </div>
+  </nav>
+
+</template>
+
+<style >
+  .navtop {
+    background-color: #000000;
+    height: 60px;
+    width: 100%;
+    border: 0;
+    position: sticky;
+}
+.navtop div {
+    display: flex;
+    margin: 0 auto;
+    width: 1000px;
+    height: 100%;
+}
+.navtop div h1, .navtop div v-btn {
+    display: inline-flex;
+    align-items: center;
+}
+.navtop div h1 {
+    flex: 1;
+    font-size: 24px;
+    padding: 0;
+    margin: 0;
+    color: #ecf0f6;
+    font-weight: normal;
+}
+.navtop div v-btn {
+    padding: 0 20px;
+    text-decoration: none;
+    color: #c5d2e5;
+    font-weight: bold;
+}
+.navtop div v-btn i {
+    padding: 2px 8px 0 0;
+}
+.navtop div v-btn:hover {
+    color: #040553;
+}
+</style>
